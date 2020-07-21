f4dd98a7a44f80f51c07185865d1a0b0dd27213c
Brian Pennington <brian@rra-llc.com>
2020-07-21 14:02:28 -0400

    video volume

--- scripts.js
+++ scripts.js
@@ -9,7 +9,8 @@
         //Video popup:  https://www.reddit.com/r/HTML/comments/31xiwa/html5_video_popup/
         function onVideoClick(theLink) {
             // removed loop
-            document.getElementById("video_pop").innerHTML = "<video autoplay playsinline id=\"the_Video\"><source src=\""+theLink+"\" type=\"video/mp4\"></video>";
+            // document.getElementById("video_pop").volume = 0.5;
+            document.getElementById("video_pop").innerHTML = "<video onloadstart=\"this.volume=.3\" autoplay id=\"the_Video\"><source src=\""+theLink+"\" type=\"video/mp4\"></video>";
             document.getElementById("video_pop").style.display="block";
         } 
 
